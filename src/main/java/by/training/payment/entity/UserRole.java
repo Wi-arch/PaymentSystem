05f9e9cbdc2d3f585c820cb599fa5c558ed7e228
Alexandr Borovets <borovets9494@mail.ru>
2020-03-05 14:20:38 +0300

    Create UserRole entity

---
+++ src/main/java/by/training/payment/entity/UserRole.java
@@ -0,0 +1,66 @@
+package by.training.payment.entity;
+
+public class UserRole {
+
+	private int id;
+	private String value;
+
+	public UserRole() {
+
+	}
+
+	public UserRole(int id, String value) {
+		this.id = id;
+		this.value = value;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserRole other = (UserRole) obj;
+		if (id != other.id)
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "UserRole [id=" + id + ", value=" + value + "]";
+	}
+
+}
