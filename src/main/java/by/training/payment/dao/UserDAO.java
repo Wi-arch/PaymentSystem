7e4dc00fa6dbac59dfb916bf2f800fd77975f187
Alexandr Borovets <borovets9494@mail.ru>
2020-03-07 14:30:45 +0300

    Create UserDAO

---
+++ src/main/java/by/training/payment/dao/UserDAO.java
@@ -0,0 +1,19 @@
+package by.training.payment.dao;
+
+import java.util.List;
+
+import by.training.payment.entity.User;
+import by.training.payment.exception.DAOException;
+
+public interface UserDAO {
+
+	void addUser(User user) throws DAOException;
+
+	void updateUser(User user) throws DAOException;
+
+	void deleteUser(User user) throws DAOException;
+
+	List<User> getAllUsers() throws DAOException;
+
+	User getUserById(int id) throws DAOException;	
+}
