7282791f1a760557633bc6320207cdc4d72b5a6d
Alexandr Borovets <borovets9494@mail.ru>
2020-03-07 14:51:52 +0300

    Create TransactionDAO

---
+++ src/main/java/by/training/payment/dao/TransactionDAO.java
@@ -0,0 +1,23 @@
+package by.training.payment.dao;
+
+import java.util.List;
+
+import by.training.payment.entity.Transaction;
+import by.training.payment.exception.DAOException;
+
+public interface TransactionDAO {
+
+	void addTransaction(Transaction transaction) throws DAOException;
+
+	void updateTransaction(Transaction transaction) throws DAOException;
+
+	void deleteTransaction(Transaction transaction) throws DAOException;
+
+	List<Transaction> getAllTransactions() throws DAOException;
+
+	Transaction geTransactionById(int id) throws DAOException;
+
+	List<Transaction> getAllTransactionsByAccountId(int accountId) throws DAOException;
+
+	List<Transaction> getAllTransactionsByCardId(int cardId) throws DAOException;
+}
