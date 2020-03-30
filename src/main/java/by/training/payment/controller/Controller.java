dd11acff3234f1c6f930d2e132cf8024dc76a9e6
Alexandr Borovets <borovets9494@mail.ru>
2020-03-30 08:30:32 +0300

    Add post request processing to Controller.java

--- src/main/java/by/training/payment/controller/Controller.java
+++ src/main/java/by/training/payment/controller/Controller.java
@@ -8,13 +8,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+
+import by.training.payment.command.PageEnum;
 import by.training.payment.exception.NoJDBCDriverException;
 import by.training.payment.exception.NoJDBCPropertiesFileException;
+import by.training.payment.factory.CommandFactory;
 import by.training.payment.pool.PoolConnection;
 
 @SuppressWarnings("serial")
 @WebServlet("/controller")
 public class Controller extends HttpServlet {
+
+	private static final Logger LOGGER = Logger.getLogger(Controller.class);
+	private static final CommandFactory COMMAND_FACTORY = new CommandFactory();
 
 	@Override
 	public void init() throws ServletException {
@@ -22,18 +29,19 @@ public class Controller extends HttpServlet {
 		try {
 			PoolConnection.INSTANCE.initializePool();
 		} catch (NoJDBCDriverException | NoJDBCPropertiesFileException e) {
-			throw new RuntimeException("Cannot initialize connection pool");
+			LOGGER.fatal("Cannot initialize connection pool", e);
 		}
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		doPost(request, response);
+		request.getRequestDispatcher(PageEnum.HOME_PAGE.getValue()).forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO
+		String path = COMMAND_FACTORY.defineCommand(request).execute(request, response);
+		request.getRequestDispatcher(path).forward(request, response);
 	}
 
 	@Override
@@ -41,5 +49,4 @@ public class Controller extends HttpServlet {
 		super.destroy();
 		PoolConnection.INSTANCE.closePool();
 	}
-
 }
