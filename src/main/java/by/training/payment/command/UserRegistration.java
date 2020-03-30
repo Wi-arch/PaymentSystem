5179fce8435fc7ccee4cd0b4691d9a2f1fe980a4
Alexandr Borovets <borovets9494@mail.ru>
2020-03-30 10:44:22 +0300

    Create UserRegistration command

---
+++ src/main/java/by/training/payment/command/UserRegistration.java
@@ -0,0 +1,28 @@
+package by.training.payment.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import by.training.payment.exception.ServiceException;
+import by.training.payment.util.ExceptionParser;
+
+public class UserRegistration extends AbstractUserCommand {
+
+	private static final Logger LOGGER = Logger.getLogger(UserRegistration.class);
+	private static final String SUCCESSFULLY_REGISTRATION_STATUS = "*Status1009*";
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) {
+		String confirmPassword = request.getParameter(RequestParameter.USER_PASSWORD_CONFIRM.toString());
+		try {
+			USER_SERVICE.registerUser(buildUser(request), confirmPassword);
+			request.setAttribute(RequestParameter.RESULT_MESSAGE.toString(), SUCCESSFULLY_REGISTRATION_STATUS);
+		} catch (ServiceException e) {
+			LOGGER.warn("Registration exception", e);
+			request.setAttribute(RequestParameter.ERROR_MESSAGE.toString(), ExceptionParser.getExceptionStatus(e));
+		}
+		return PageEnum.REGISTRATION_PAGE.getValue();
+	}
+}
