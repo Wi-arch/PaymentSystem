f32bc900a76df0a2fb4408af1c74434cdfe8c725
Alexandr Borovets <borovets9494@mail.ru>
2020-03-30 08:40:33 +0300

    Create ExceptionParser class

---
+++ src/main/java/by/training/payment/util/ExceptionParser.java
@@ -0,0 +1,24 @@
+package by.training.payment.util;
+
+public class ExceptionParser {
+
+	private static final String DEFAULT_STATUS = "*Status999*";
+	private static final String STATUS_REGEX = ".*(\\*Status\\d+\\*).*";
+
+	public static String getExceptionStatus(Throwable e) {
+		if (e == null) {
+			return DEFAULT_STATUS;
+		}
+		String status = getSourceCause(e).toString();
+		return status.matches(STATUS_REGEX) ? status.replaceAll(STATUS_REGEX, "$1") : DEFAULT_STATUS;
+	}
+
+	private static Throwable getSourceCause(Throwable e) {
+		Throwable cause = null;
+		Throwable result = e;
+		while ((cause = result.getCause()) != null && (result != cause)) {
+			result = cause;
+		}
+		return result;
+	}
+}
