f03a3cdf38abf90343267fbbbac63ea04c636124
Alexandr Borovets <borovets9494@mail.ru>
2020-03-30 08:43:14 +0300

    Create SessionLoginFilter

---
+++ src/main/java/by/training/payment/filter/SessionLoginFilter.java
@@ -0,0 +1,35 @@
+package by.training.payment.filter;
+
+import java.io.IOException;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import by.training.payment.command.PageEnum;
+import by.training.payment.command.RequestParameter;
+import by.training.payment.entity.User;
+
+@WebFilter(dispatcherTypes = { DispatcherType.REQUEST, DispatcherType.FORWARD, DispatcherType.INCLUDE }, urlPatterns = {
+		"/jsp/admin/*", "/jsp/user/*" })
+public class SessionLoginFilter implements Filter {
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpServletResponse httpsResponse = (HttpServletResponse) response;
+		User user = (User) httpRequest.getSession().getAttribute(RequestParameter.USER.toString());
+		if (user == null || user.isBlocked()) {
+			httpsResponse.sendRedirect(httpRequest.getContextPath() + PageEnum.LOGIN_PAGE.getValue());
+			return;
+		}
+		chain.doFilter(request, response);
+	}
+
+}
