c4d6966865e312de5397512e3b9de0689a5261c9
Alexandr Borovets <borovets9494@mail.ru>
2020-03-30 15:15:51 +0300

    Add ShowUserRequestMenu command to CommandFactory

--- src/main/java/by/training/payment/factory/CommandFactory.java
+++ src/main/java/by/training/payment/factory/CommandFactory.java
@@ -1,6 +1,6 @@
 package by.training.payment.factory;
 
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,22 +8,24 @@ import javax.servlet.http.HttpServletRequest;
 import by.training.payment.command.Command;
 import by.training.payment.command.CommandName;
 import by.training.payment.command.RequestParameter;
+import by.training.payment.command.RestoreUserPassword;
+import by.training.payment.command.UserAuthorization;
+import by.training.payment.command.UserLogin;
+import by.training.payment.command.UserLogout;
+import by.training.payment.command.UserRegistration;
 import by.training.payment.command.language.LanguageCommand;
 import by.training.payment.command.user.DeleteUserAccount;
-import by.training.payment.command.user.RestoreUserPassword;
+import by.training.payment.command.user.ShowMenuForCreatingRequest;
 import by.training.payment.command.user.ShowUserCurrenciesMenu;
 import by.training.payment.command.user.ShowUserDataMenu;
+import by.training.payment.command.user.ShowUserRequestsMenu;
 import by.training.payment.command.user.UpdateUserName;
 import by.training.payment.command.user.UpdateUserPassword;
 import by.training.payment.command.user.UpdateUserSurname;
-import by.training.payment.command.user.UserAuthorization;
-import by.training.payment.command.user.UserLogin;
-import by.training.payment.command.user.UserLogout;
-import by.training.payment.command.user.UserRegistration;
 
 public class CommandFactory {
 
-	private final Map<CommandName, Command> commandMap = new HashMap<>();
+	private final Map<CommandName, Command> commandMap = new EnumMap<>(CommandName.class);
 
 	public CommandFactory() {
 		commandMap.put(CommandName.REGISTRATION, new UserRegistration());
@@ -38,7 +40,8 @@ public class CommandFactory {
 		commandMap.put(CommandName.UPDATE_USER_SURNAME, new UpdateUserSurname());
 		commandMap.put(CommandName.UPDATE_USER_PASSWORD, new UpdateUserPassword());
 		commandMap.put(CommandName.DELETE_USER_ACCOUNT, new DeleteUserAccount());
-		// TODO put more...
+		commandMap.put(CommandName.SHOW_ALL_USER_REQUESTS, new ShowUserRequestsMenu());
+		commandMap.put(CommandName.SHOW_MENU_FOR_CREATING_REQUESTS, new ShowMenuForCreatingRequest());
 	}
 
 	public Command defineCommand(HttpServletRequest request) {
