1fe3f6daef1d053a435302366ac28043012f21b6
Alexandr Borovets <borovets9494@mail.ru>
2020-04-08 19:57:09 +0300

    Create CardValidator

---
+++ src/main/java/by/training/payment/validator/CardValidator.java
@@ -0,0 +1,55 @@
+package by.training.payment.validator;
+
+import static org.apache.commons.codec.digest.DigestUtils.sha1Hex;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import by.training.payment.entity.Card;
+import by.training.payment.exception.ServiceException;
+
+public class CardValidator {
+
+	private static final int ONE = 1;
+
+	public void checkIsAmountPositive(BigDecimal amount) throws ServiceException {
+		if (amount == null || amount.compareTo(BigDecimal.ZERO) < ONE) {
+			throw new ServiceException("Amount must be greater than zero *Status1019*");
+		}
+	}
+
+	public void compareSenderAndReceiverCardNumber(String senderNumber, String receiverNumber) throws ServiceException {
+		if (senderNumber != null && senderNumber.equals(receiverNumber)) {
+			throw new ServiceException("Same sender and receiver card numbers *Status1025*");
+		}
+	}
+
+	public void checkIsCardBlocked(Card card) throws ServiceException {
+		if (card == null || card.getIsBlocked()) {
+			throw new ServiceException("Card is blocked *Status1020*");
+		}
+	}
+
+	public void checkCardNumberForNull(Card card) throws ServiceException {
+		if (card == null || card.getNumber() == null) {
+			throw new ServiceException("Null card number *Status1024*");
+		}
+	}
+
+	public void checkIsPaymentPurposeEmpty(String paymentPurpose) throws ServiceException {
+		if (paymentPurpose == null || paymentPurpose.isEmpty()) {
+			throw new ServiceException("Null payment purpose");
+		}
+	}
+
+	public void compareCcvCodes(String userCcvCode, String cardCcvCode) throws ServiceException {
+		if (userCcvCode == null || !sha1Hex(userCcvCode).equals(cardCcvCode)) {
+			throw new ServiceException("Wrong ccv code *Status1022*");
+		}
+	}
+
+	public boolean isExpiredDateCard(Card card) {
+		return card != null && card.getValidUntilDate() != null && card.getValidUntilDate().after(new Date());
+	}
+
+}
