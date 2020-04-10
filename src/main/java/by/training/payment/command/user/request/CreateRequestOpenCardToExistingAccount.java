01a06bc0d7e045707e18223ca257ad4ba4a4fd04
Alexandr Borovets <borovets9494@mail.ru>
2020-04-10 16:20:22 +0300

    Add CreateRequestOpenCardToExistingAccount command

--- src/main/java/by/training/payment/command/user/request/CreateRequestOpenCardToExistingAccount.java
+++ src/main/java/by/training/payment/command/user/request/CreateRequestOpenCardToExistingAccount.java
@@ -8,7 +8,7 @@ import static by.training.payment.factory.RequestTypeFactory.OPEN_NEW_CARD_TO_EX
 import by.training.payment.command.PageEnum;
 import by.training.payment.command.RequestParameter;
 import by.training.payment.entity.BankAccount;
-import by.training.payment.entity.CardExpirationDate;
+import by.training.payment.entity.CardExpiry;
 import by.training.payment.entity.PaymentSystem;
 import by.training.payment.entity.RequestType;
 import by.training.payment.entity.UserRequest;
@@ -25,7 +25,7 @@ public class CreateRequestOpenCardToExistingAccount extends AbstractCreateReques
 		BankAccount bankAccount = new BankAccount(request.getParameter(RequestParameter.BANK_ACCOUNT_NUMBER.toString()));
 		PaymentSystem paymentSystem = new PaymentSystem(request.getParameter(RequestParameter.PAYMENT_SYSTEM.toString()));
 		String cardExpirationDateValue = request.getParameter(RequestParameter.CARD_EXPIRATION_DATE.toString());
-		CardExpirationDate cardExpirationDate = new CardExpirationDate(Integer.valueOf(cardExpirationDateValue));
+		CardExpiry cardExpirationDate = new CardExpiry(Integer.valueOf(cardExpirationDateValue));
 		UserRequest userRequest = createUserRequest(request, openNewCardToExistingAccount);
 		try {
 			userRequestService.saveRequestToOpenNewCardToExistingAccount(userRequest, bankAccount, paymentSystem, cardExpirationDate);
