890d9f57e95e981f142600851a4f44396a43aaae
Alexandr Borovets <borovets9494@mail.ru>
2020-04-12 13:07:06 +0300

    Create ShowAllCardsByUserLogin command

---
+++ src/main/java/by/training/payment/command/admin/card/ShowAllCards.java
@@ -0,0 +1,32 @@
+package by.training.payment.command.admin.card;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import by.training.payment.command.PageEnum;
+import by.training.payment.command.RequestParameter;
+import by.training.payment.command.user.card.AbstractCardCommand;
+import by.training.payment.entity.Card;
+import by.training.payment.exception.ServiceException;
+import by.training.payment.util.ExceptionParser;
+
+public class ShowAllCards extends AbstractCardCommand {
+
+	private static final Logger LOGGER = Logger.getLogger(ShowAllCards.class);
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) {
+		try {
+			List<Card> cardList = cardService.getAllCards();
+			setCardListInRequestAttribute(request, cardList);
+		} catch (ServiceException e) {
+			request.setAttribute(RequestParameter.ERROR_MESSAGE.toString(), ExceptionParser.getExceptionStatus(e));
+			LOGGER.warn("Cannot load card list", e);
+		}
+		return PageEnum.ADMIN_CARDS_MENU.getValue();
+	}
+}
---
+++ src/main/java/by/training/payment/command/admin/card/ShowAllCardsByUserLogin.java
@@ -0,0 +1,33 @@
+package by.training.payment.command.admin.card;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import by.training.payment.command.PageEnum;
+import by.training.payment.command.RequestParameter;
+import by.training.payment.command.user.card.AbstractCardCommand;
+import by.training.payment.entity.Card;
+import by.training.payment.exception.ServiceException;
+import by.training.payment.util.ExceptionParser;
+
+public class ShowAllCardsByUserLogin extends AbstractCardCommand {
+
+	private static final Logger LOGGER = Logger.getLogger(ShowAllCardsByUserLogin.class);
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) {
+		String login = request.getParameter(RequestParameter.USER_LOGIN.toString());
+		try {
+			List<Card> cardList = cardService.getAllCardsByUserLogin(login);
+			setCardListInRequestAttribute(request, cardList);
+		} catch (ServiceException e) {
+			request.setAttribute(RequestParameter.ERROR_MESSAGE.toString(), ExceptionParser.getExceptionStatus(e));
+			LOGGER.warn("Cannot load card list", e);
+		}
+		return PageEnum.ADMIN_CARDS_MENU.getValue();
+	}
+}
