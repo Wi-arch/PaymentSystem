730e16ab8a0a9d31a71f812725c2bccfa3e2c4c7
Alexandr Borovets <borovets9494@mail.ru>
2020-04-13 14:41:12 +0300

    Create java documentation comments to RequestParameterValidator

--- src/main/java/by/training/payment/validator/RequestParameterValidator.java
+++ src/main/java/by/training/payment/validator/RequestParameterValidator.java
@@ -13,13 +13,42 @@ import by.training.payment.entity.RequestParameter;
 import by.training.payment.exception.ServiceException;
 import by.training.payment.factory.ParameterHeaderFactory;
 
+/**
+ * The {@link RequestParameterValidator} class validates
+ * {@link RequestParameter} entity.
+ * 
+ * @author Alexandr Borovets
+ * @since JDK1.8
+ */
 public class RequestParameterValidator {
 
+	/** Field containing {@link ParameterHeaderFactory} instance */
 	private final ParameterHeaderFactory headerFactory = ParameterHeaderFactory.getInstance();
+
+	/**
+	 * Field containing {@link ParameterHeaderFactory#CARD_EXPIRY_PARAMETER_HEADER}
+	 * value
+	 */
 	private final ParameterHeader cardExpiryHeader = headerFactory.getParameterHeader(CARD_EXPIRY_PARAMETER_HEADER);
+
+	/**
+	 * Field containing
+	 * {@link ParameterHeaderFactory#PAYMENT_SYSTEM_PARAMETER_HEADER} value
+	 */
 	private final ParameterHeader paymentSystemHeader = headerFactory.getParameterHeader(PAYMENT_SYSTEM_PARAMETER_HEADER);
+
+	/**
+	 * Field containing
+	 * {@link ParameterHeaderFactory#CURRENCY_NAME_PARAMETER_HEADER} value
+	 */
 	private final ParameterHeader currencyHeader = headerFactory.getParameterHeader(CURRENCY_NAME_PARAMETER_HEADER);
 
+	/**
+	 * 
+	 * @param requestParameterList {@link List<RequestParameter>} to be validated
+	 * @throws ServiceException if {@link List<RequestParameter>} null or one of
+	 *                          {@link List<RequestParameter>} elements null
+	 */
 	public void checkRequestParameterListForNull(List<RequestParameter> requestParameterList) throws ServiceException {
 		if (requestParameterList == null) {
 			throw new ServiceException("Null request parameter list");
@@ -29,6 +58,13 @@ public class RequestParameterValidator {
 		}
 	}
 
+	/**
+	 * 
+	 * @param parameter {@link RequestParameter} to be validated
+	 * @throws ServiceException if {@link RequestParameter} null or
+	 *                          {@link RequestParameter} is not
+	 *                          {@link RequestParameterValidator#cardExpiryHeader}
+	 */
 	public void checkIsCardExpiryRequestParameter(RequestParameter parameter) throws ServiceException {
 		checkRequestParameterValueForNull(parameter);
 		if (!cardExpiryHeader.equals(parameter.getParameterHeader()) || !StringUtils.isNumeric(parameter.getValue())) {
@@ -36,6 +72,13 @@ public class RequestParameterValidator {
 		}
 	}
 
+	/**
+	 * 
+	 * @param parameter {@link RequestParameter} to be validated
+	 * @throws ServiceException if {@link RequestParameter} null or
+	 *                          {@link RequestParameter} is not
+	 *                          {@link RequestParameterValidator#paymentSystemHeader}
+	 */
 	public void checkIsPaymentSystemRequestParameter(RequestParameter parameter) throws ServiceException {
 		checkRequestParameterValueForNull(parameter);
 		if (!paymentSystemHeader.equals(parameter.getParameterHeader())) {
@@ -43,6 +86,13 @@ public class RequestParameterValidator {
 		}
 	}
 
+	/**
+	 * 
+	 * @param parameter {@link RequestParameter} to be validated
+	 * @throws ServiceException if {@link RequestParameter} null or
+	 *                          {@link RequestParameter} is not
+	 *                          {@link RequestParameterValidator#currencyHeader}
+	 */
 	public void checkIsCurrencyRequestParameter(RequestParameter parameter) throws ServiceException {
 		checkRequestParameterValueForNull(parameter);
 		if (!currencyHeader.equals(parameter.getParameterHeader())) {
@@ -50,6 +100,13 @@ public class RequestParameterValidator {
 		}
 	}
 
+	/**
+	 * 
+	 * @param parameter {@link RequestParameter} to be validated
+	 * @throws ServiceException if {@link RequestParameter} null or
+	 *                          {@link RequestParameter#getValue()} return null
+	 *                          value
+	 */
 	public void checkRequestParameterValueForNull(RequestParameter parameter) throws ServiceException {
 		if (parameter == null || parameter.getValue() == null) {
 			throw new ServiceException("Null request parameter value");
