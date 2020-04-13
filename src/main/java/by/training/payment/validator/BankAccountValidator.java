513e91add8e75ab0663ae49c2713f73010050463
Alexandr Borovets <borovets9494@mail.ru>
2020-04-13 15:11:45 +0300

    Create java documentation comments to BankAccountValidator

--- src/main/java/by/training/payment/validator/BankAccountValidator.java
+++ src/main/java/by/training/payment/validator/BankAccountValidator.java
@@ -3,14 +3,31 @@ package by.training.payment.validator;
 import by.training.payment.entity.BankAccount;
 import by.training.payment.exception.ServiceException;
 
+/**
+ * The {@link BankAccountValidator} class validates {@link BankAccount} entity.
+ * 
+ * @author Alexandr Borovets
+ * @since JDK1.8
+ */
 public class BankAccountValidator {
 
+	/**
+	 * @param bankAccount {@link BankAccount} to be validated
+	 * @throws ServiceException if {@link BankAccount} null or
+	 *                          {@link BankAccount#getIsBlocked()} return true
+	 */
 	public void checkIsBankAccountBlocked(BankAccount bankAccount) throws ServiceException {
 		if (bankAccount == null || bankAccount.getIsBlocked()) {
 			throw new ServiceException("Bank account is blocked *Status1021*");
 		}
 	}
 
+	/**
+	 * @param bankAccount {@link BankAccount} to be validated
+	 * @throws ServiceException if {@link BankAccount} null or
+	 *                          {@link BankAccount#getAccountNumber()} return null
+	 *                          value
+	 */
 	public void checkBankAccountNumberForNull(BankAccount bankAccount) throws ServiceException {
 		if (bankAccount == null || bankAccount.getAccountNumber() == null) {
 			throw new ServiceException("Null bank account number");
