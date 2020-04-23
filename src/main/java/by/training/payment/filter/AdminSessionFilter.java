630fcb787b11797caae144566ede7ac1decb252a
Alexandr Borovets <borovets9494@mail.ru>
2020-04-23 16:48:40 +0300

    Create java documentation comments to AdminSessionFilter

--- src/main/java/by/training/payment/filter/AdminSessionFilter.java
+++ src/main/java/by/training/payment/filter/AdminSessionFilter.java
@@ -16,6 +16,15 @@ import by.training.payment.command.PageEnum;
 import by.training.payment.command.RequestParameter;
 import by.training.payment.entity.User;
 
+/**
+ * The filter is designed to prevent access to pages in the jsp/admin directory
+ * to users who are not administrators or blocked users. When you try to access
+ * these pages, you are redirected to the login page
+ * {@link PageEnum#LOGIN_PAGE}.
+ * 
+ * @author Alexandr Borovets
+ * @since JDK1.8
+ */
 @WebFilter(filterName = "AdminSessionFilter", dispatcherTypes = { DispatcherType.REQUEST, DispatcherType.FORWARD,
 		DispatcherType.INCLUDE }, urlPatterns = { "/jsp/admin/*" })
 public class AdminSessionFilter implements Filter {
